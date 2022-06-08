606d9e52044b22116095e2aac9d253c01ef4a5cc
Jose Castillo <jmcg0901@gmail.com>
2022-06-08 07:32:19 -0500

    Modify config.js

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 // Load MySql pool connection
 const pool = require('./data/config')
 
+// Body parser
 const bodyParser = require('body-parser')
 
+// Express
 const express = require('express')
 const { response } = require('express')
 const app = express()
--- data/config.js
+++ data/config.js
@@ -7,6 +7,13 @@ const config = {
     database: 'reto_tc2005b'
 }
 
-const pool = mysql.createPool(config)
+const config_azure = {
+    host: 'db-tc2005b.mysql.database.azure.com',
+    user: 'jmcg0901',
+    password: 'Yeahboi0901$',
+    database: 'reto_tc2005b'
+}
+
+const pool = mysql.createPool(config_azure)
 
 module.exports = pool
